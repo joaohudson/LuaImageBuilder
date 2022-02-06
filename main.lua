e93c02195efb490a0d787a77bc1f3481474b29eb
João Hudson <joao.oliveira@projecti.com.br>
2022-02-05 21:51:29 -0300

    mudando exemplo de imagem

--- main.lua
+++ main.lua
@@ -4,26 +4,15 @@ local util = require "lib/util"
 -- y - is vertical coordinate between 0 and 1
 -- aspect - is height/width of the image
 function imageBuilder(x, y, aspect)
-	
+	-- set origin in center screen
 	x = x - 0.5
 	y = y - 0.5
-	
-	local r = math.sqrt(x * x + y * y)
-	local teta = math.atan2(y, x)
-	
-	local l = 1 - math.sqrt(x * x + y * y) / math.sqrt(0.5)
-	
-	x = x + 0.5
-	y = y + 0.5
-	
-	local red = x * x * x * 1.5
-	local green = r * r * 4
-	local blue = (1 - r) * (1 - r) * 1.5
-	
-	l = l * l * l
-	local mask = (math.sin(teta * 16 + r * 64) * 0.5 + 0.5)
-	
-	return util.newColor(l * red * mask, l * green * mask, l * blue * mask)
+	-- 1 - radius = bigger glow in the center
+	local l = 1 - math.sqrt(x*x + y*y)
+	-- l*l*l... = exponential that amplifies the focus of light
+	local r = l*l*l*l*l
+
+	return util.newColor(r,r,r)
 end
 
 return imageBuilder
